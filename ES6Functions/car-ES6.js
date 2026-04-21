f5192b07f1af91e6f79bfbc6a1f49fad081beb02
Lucas Guaragna <37838183+lucasguaragna@users.noreply.github.com>
2026-04-21 12:48:29 -0300

    ES6 studies

--- ES6Functions/car-ES6.js
+++ ES6Functions/car-ES6.js
@@ -6,17 +6,37 @@ class CarCl {
         this.speed = speed;
     } 
 
-    // Methods will be linked to the prototype
+    // Methods will added to the prototype property
     accelerate(){
-        accelerate = 10;
+        const accelerate = 10;
         this.speed += accelerate;
-        console.log(this.speed);
+        console.log(`${this.make}'s speed = ${this.speed}km/h (accelerated by ${accelerate}km/h)`);
     }
 
     brake(){
-        brake = 5;
+        const brake = 5;
         this.speed -= brake;
-        console.log(this.speed);
+        console.log(`${this.make}'s speed = ${this.speed}km/h (brake by ${brake}km/h)`);
+    }
+
+    // new property
+    get speedUS(){
+        return `${this.speed / 1.6}mi/h`;
+    }
+
+    set speedUS(num){
+        this.speed = num * 1.6;
     }
 }
+
+const ford = new CarCl('ford', 120)
+
+ford.accelerate()
+ford.accelerate()
+ford.brake()
+ford.accelerate()
+
+ford.speedUS = 50;
+console.log(ford)
+
 
