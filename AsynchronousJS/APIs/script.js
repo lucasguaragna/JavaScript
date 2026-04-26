a3f6ad32c1004950fc6ee5596e7b080b01bb417f
Lucas Guaragna <37838183+lucasguaragna@users.noreply.github.com>
2026-04-26 19:23:35 -0300

    asynchronous JS studies and challenge

---
+++ AsynchronousJS/APIs/script.js
@@ -0,0 +1,42 @@
+// 'use strict';
+const getCountryData = function(countryName){
+    const btn = document.querySelector('.btn-country');
+    const countriesContainer = document.querySelector('.countries')
+
+
+    ///////
+
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v2/name/${countryName}`);
+    request.send();
+
+    // send request that fetches data in the background
+    // once is done, it will emit the "load" event
+    // so we listen to it:
+
+    request.addEventListener('load', function(){
+        // console.log(this.responseText)
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+
+        const html = `
+            <article class="country">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)}</p>
+                <p class="country__row"><span>🗣️</span>${data.languages[0].nativeName}</p>
+                <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+            </div>
+            </article>
+        `;
+
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+        countriesContainer.style.opacity = 1;
+
+    });
+};
+
+getCountryData('brazil');
+
