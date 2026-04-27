803ddbca523830c56ac8d5ec4bb33143b45877fe
Lucas Guaragna <37838183+lucasguaragna@users.noreply.github.com>
2026-04-27 20:12:53 -0300

    try catch and async/await

--- AsynchronousJS/APIs/script.js
+++ AsynchronousJS/APIs/script.js
@@ -1,42 +1,140 @@
-// 'use strict';
-const getCountryData = function(countryName){
-    const btn = document.querySelector('.btn-country');
-    const countriesContainer = document.querySelector('.countries')
+// // 'use strict';
+
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries')
+
+// const renderCountry = function(data, className = ''){
+//     const html = `
+//     <article class="country">
+//         <img class="country__img" src="${data.flag}" />
+//         <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)}</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].nativeName}</p>
+//             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//         </div>
+//     </article>
+//     `;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+// };
+
+//     const request = new XMLHttpRequest();
+//     request.open('GET', `https://restcountries.com/v2/name/${countryName}`);
+//     request.send();
+
+//     // send request that fetches data in the background
+//     // once is done, it will emit the "load" event
+//     // so we listen to it:
+
+//     request.addEventListener('load', function(){
+//         // console.log(this.responseText)
+//         const [data] = JSON.parse(this.responseText);
+//         console.log(data);
+//         // Render Country
+//         renderCountry(data);
+//     });
+// };
+
+// getCountryAndNeighbour('brazil');
+
+// USING PROMISES
+// const getCountryData = function(country){
+//     fetch(`https://restcountries.com/v2/name/${country}`)
+//         .then(function(response){
+//             return response.json(); //it returns another promise
+//         })
+//         .then(function(data){
+//             renderCountry(data[0])
+//        })
+//        .catch(err => alert(err));
+// };
+
+// btn.addEventListener('click', function(){
+//     getCountryData('brazil');
+// });
 
 
-    ///////
+// Premisifying navigator.geolocation.getCurrentPosition
+const getPosition = function(){
+    return new Promise(function(resolve, reject){
+        // navigator.geolocation.getCurrentPosition(
+        //     position => resolve(position), 
+        //     err => reject(err)
+        // );
+        navigator.geolocation.getCurrentPosition(resolve,reject);
 
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v2/name/${countryName}`);
-    request.send();
+    })
 
-    // send request that fetches data in the background
-    // once is done, it will emit the "load" event
-    // so we listen to it:
+}
 
-    request.addEventListener('load', function(){
-        // console.log(this.responseText)
-        const [data] = JSON.parse(this.responseText);
-        console.log(data);
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries')
 
-        const html = `
-            <article class="country">
-            <img class="country__img" src="${data.flag}" />
-            <div class="country__data">
-                <h3 class="country__name">${data.name}</h3>
-                <h4 class="country__region">${data.region}</h4>
-                <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)}</p>
-                <p class="country__row"><span>🗣️</span>${data.languages[0].nativeName}</p>
-                <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-            </div>
-            </article>
-        `;
+// const WhereAmI = function(){
+//     getPosition().then(pos => {
+//         const {latitude: lat, longitude: lng} = pos.coords;
 
-        countriesContainer.insertAdjacentHTML('beforeend', html);
-        countriesContainer.style.opacity = 1;
+//         return fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`)
+//     })
+//         .then(res => {
+//             if (!res.ok) throw new Error(`Problem with geocoding: ${res.statusText} (${res.status})`)
+//             return res.json()
+//         })
+//         .then(data => {
+//             console.log(`You are in ${data.city}, ${data.countryName}`);
 
-    });
+//             return fetch(`https://restcountries.com/v2/name/${data.countryName}`)
+//         })
+//         .then(res => {
+//             if(!res.ok) throw new Error(`Country not found (${res.status})`);
+//             return res.json();
+//         })
+//         .then(data => renderCountry(data[0]))
+//         .catch(err => console.error(`🤯 ${error.message}`))
+// }
+   
+const whereAmI = async function(){
+    try {
+        const pos = await getPosition(); // Returns a Promise
+        const {latitude: lat, longitude: lng} = pos.coords;
+    
+        const resgeo = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`)
+        if (!resgeo.ok) throw new Error(`Problem with geocoding: ${resgeo.status}`)
+        const datageo = await resgeo.json()
+    
+        const res = await fetch(`https://restcountries.com/v2/name/${datageo.countryName}`)
+        const data = await res.json()
+    
+        renderCountry(data[0])
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const renderCountry = function(data, className = ''){
+    const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].nativeName}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+    </article>
+    `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
 };
 
-getCountryData('brazil');
+btn.addEventListener('click', function(){
+    whereAmI();
+});
+
+
 
---
+++ AsynchronousJS/challenges/challenge-2/script.js
@@ -0,0 +1,85 @@
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries')
+// const WhereAmI = function(lat, lng){
+//     fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`)
+//         .then(res => {
+//             if (!res.ok) throw new Error(`Problem with geocoding: ${res.statusText} (${res.status})`)
+//             return res.json()
+//         })
+//         .then(data => {
+//             console.log(`You are in ${data.city}, ${data.countryName}`);
+
+//             return fetch(`https://restcountries.com/v2/name/${data.countryName}`)
+//         })
+//         .then(res => {
+//             if(!res.ok) throw new Error(`Country not found (${res.status})`);
+//             return res.json();
+//         })
+//         .then(data => renderCountry(data[0]))
+//         .catch(err => console.error(`🤯 ${error.message}`))
+// }
+
+// const renderCountry = function(data, className = ''){
+//     const html = `
+//     <article class="country">
+//         <img class="country__img" src="${data.flag}" />
+//         <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)}</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].nativeName}</p>
+//             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//         </div>
+//     </article>
+//     `;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+// };
+
+// btn.addEventListener('click', function(){
+//     WhereAmI(52.508,13.381);
+// });
+
+
+const imgContainer = document.querySelector('.images');
+
+
+const createImage = function(imgPath){
+    return new Promise(function(resolve, reject){
+         const img = document.createElement('img');
+         img.src = imgPath;
+         img.addEventListener('load', function(){
+            imgContainer.append(img);
+            resolve();
+         });
+
+         img.addEventListener('error', function(){
+            reject(new Error('Image not found'));
+         })
+    })
+}
+
+let currentImg
+
+createImage('img/img-1.jpeg')
+    .then(img => {
+        currentImg = img;
+        console.log('Image 1 loaded')
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+        return createImage('img/img-2.jpeg')
+    })
+    .then(img => {
+        currentImg = img;
+        console.log('Image 2 loaded')
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+    })
+    .catch(err => console.error(err))
+
+
